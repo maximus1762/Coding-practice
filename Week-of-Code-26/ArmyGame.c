8a78ca94328314d78fa1a3e200e69f2cbd50fba0
raj2001762 <raj2001762@users.noreply.github.com>
2016-11-29 15:55:54 +0530

    Create ArmyGame.c
    https://www.hackerrank.com/contests/w26/challenges/game-with-cells/

---
+++ Week-of-Code-26/ArmyGame.c
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n; 
+    int m; 
+    scanf("%d %d",&n,&m);
+    int a=(n+1)/2;
+    int b=(m+1)/2;
+    printf("%d",a*b);
+    return 0;
+}
